8637a0224d8f026895cc43d0e6302c133c9e50b1
Daniel_Padgett <danpadge16@gmail.com>
2021-04-09 15:16:15 +1000

    js error fix

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,13 @@ $(document).ready(function(){
         var city = $("#city").val();
         if(city != ''){
             $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "APPID=2368c2726863bce51fad785625b8efbe",
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=2368c2726863bce51fad785625b8efbe",
                 type: "GET",
-                dataType: "jsonp"
+                dataType: "jsonp",
+                success: function(data){
+                    console.log(data);
+
+                }
 
             });
 
